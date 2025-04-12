ac14d55f9b94f6805e22942731a619910a0316df
YukiniSakuhana <ysu3286@gmail.com>
2025-04-12 23:28:16 +0800

    准备从windows迁移linux了，之后以这边的学习为主

---
+++ cmake-build/apps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yukinisakuhana/program/cs144/apps
+# Build directory: /home/yukinisakuhana/program/cs144/cmake-build/apps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ cmake-build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yukinisakuhana/program/cs144/src
+# Build directory: /home/yukinisakuhana/program/cs144/cmake-build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
